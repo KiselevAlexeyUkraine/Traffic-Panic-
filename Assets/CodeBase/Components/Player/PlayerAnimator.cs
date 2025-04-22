19b189a2ace0becb65cbeda69a431f9073f889a6
KiselevAlexey <alekskisliy@gmail.com>
2025-04-22 20:24:54 +0300

    Установил твинны и настроил анимацию для поворотов

---
+++ Assets/CodeBase/Components/Player/PlayerAnimator.cs
@@ -0,0 +1,57 @@
+using Codebase.Components.Player;
+using Codebase.Services.Inputs;
+using DG.Tweening;
+using UnityEngine;
+using Zenject;
+
+namespace Codebase.Components.Player
+{
+    public class PlayerAnimator : MonoBehaviour
+    {
+        [SerializeField] private Transform _carBody;
+        [SerializeField] private float _tiltYAngle = 10f;
+        [SerializeField] private float _tiltDuration = 0.25f;
+
+        private IInput _playerInput;
+        private PlayerMovement _playerMovement;
+        private Tween _tiltTween;
+
+        [Inject]
+        private void Construct(IInput input)
+        {
+            _playerInput = input;
+        }
+
+        private void Start()
+        {
+            _playerMovement = GetComponent<PlayerMovement>();
+            _playerMovement.OnMoving += HandleLean;
+        }
+
+        private void HandleLean()
+        {
+            _tiltTween?.Kill();
+
+            float targetY = 0f;
+
+            if (_playerInput.Right)
+                targetY = _tiltYAngle;
+            else if (_playerInput.Left)
+                targetY = -_tiltYAngle;
+
+            _tiltTween = _carBody.DOLocalRotate(
+                new Vector3(0f, targetY, 0f), _tiltDuration)
+                .SetEase(Ease.OutSine)
+                .OnComplete(() =>
+                {
+                    _tiltTween = _carBody.DOLocalRotate(Vector3.zero, _tiltDuration)
+                        .SetEase(Ease.OutSine);
+                });
+        }
+
+        private void OnDestroy()
+        {
+            _playerMovement.OnMoving -= HandleLean;
+        }
+    }
+}
