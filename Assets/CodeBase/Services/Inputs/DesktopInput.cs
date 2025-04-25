6e8135ed5fcf7e8ff7d689cb64d388a3eda382fc
KiselevAlexey <alekskisliy@gmail.com>
2025-04-25 14:08:23 +0300

    Создал ускорение и замедление машины
    Управляю временем на определенное количество времени

--- Assets/CodeBase/Services/Inputs/DesktopInput.cs
+++ Assets/CodeBase/Services/Inputs/DesktopInput.cs
@@ -6,7 +6,7 @@ namespace Codebase.Services.Inputs
     {
         public bool Left => Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow);
         public bool Right => Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow);
-        public bool Drag => Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow);
+        public bool Drag => Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.DownArrow);
         public bool Boost => Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow);
         public bool Action => Input.GetKeyDown(KeyCode.E);
         public bool Pause => Input.GetKeyDown(KeyCode.Escape);
