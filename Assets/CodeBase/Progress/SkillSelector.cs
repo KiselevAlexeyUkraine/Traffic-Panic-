a77ade14f4207edd871a498117b7b244d64d9a34
KiselevAlexey <alekskisliy@gmail.com>
2025-04-30 19:10:41 +0300

    Улучшил визуал с выбором скила и добавил сохранения для выбора скила

--- Assets/CodeBase/Progress/SkillSelector.cs
+++ Assets/CodeBase/Progress/SkillSelector.cs
@@ -17,9 +17,24 @@ namespace Codebase.UI
             otherButton.onClick.AddListener(() => SelectSkill(SkillSelectorPersistent.SkillType.Other));
         }
 
+        private void Start()
+        {
+            UpdateButtonStates();
+        }
+
         private void SelectSkill(SkillSelectorPersistent.SkillType skill)
         {
             SkillSelectorPersistent.Instance.SelectSkill(skill);
+            UpdateButtonStates();
+        }
+
+        private void UpdateButtonStates()
+        {
+            var selected = SkillSelectorPersistent.Instance.SelectedSkill;
+
+            armorButton.interactable = selected != SkillSelectorPersistent.SkillType.Armor;
+            magnetButton.interactable = selected != SkillSelectorPersistent.SkillType.Magnet;
+            otherButton.interactable = selected != SkillSelectorPersistent.SkillType.Other;
         }
     }
 }
--- Assets/CodeBase/Progress/SkillSelectorPersistent.cs
+++ Assets/CodeBase/Progress/SkillSelectorPersistent.cs
@@ -26,12 +26,20 @@ namespace Codebase.Services
 
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            LoadSelectedSkill();
         }
 
         public void SelectSkill(SkillType skill)
         {
             SelectedSkill = skill;
+            PlayerPrefs.SetInt("SelectedSkill", (int)skill);
+            PlayerPrefs.Save();
             Debug.Log("[SkillSelector] Selected: " + skill);
+        }
+
+        private void LoadSelectedSkill()
+        {
+            SelectedSkill = (SkillType)PlayerPrefs.GetInt("SelectedSkill", 0);
         }
     }
 }
